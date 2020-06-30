902045c6893b13a78bbf4556187cc3201066a0e3
davegregg@gmail.com <davegregg@gmail.com>
2020-06-30 14:41:39 -0400

    Init

---
+++ main.js
@@ -0,0 +1,7 @@
+// NOTE: A "users" array is already loaded.
+// See the "./data/users.js" script tag in index.html.
+
+// The following line is here just to show you that the
+// "users" array has already been loaded and is ready to go.
+console.log(users)
+
