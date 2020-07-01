1b33bc4a2f504702840c110e13dc3df241e04ce3
AntMan <xanthonyalvarezx@gmail.com>
2020-06-30 21:37:44 -0400

    initial commit

--- main.js
+++ main.js
@@ -7,3 +7,17 @@ console.log(users)
 
 // Append the katas to this element:
 const main = document.querySelector('main')
+
+kata6 = document.createElement('h1')
+summary6 = document.createElement('summary')
+details6 = document.createElement('details')
+kata6.append('kata6')
+summary6.append(kata6)
+details6.append(summary6)
+main.append(details6)
+
+let personBalance =[]
+let userBalance = users.filter(user => user.company === "ZENCO").map(user => user.balance)
+personBalance.push(userBalance)
+console.log({personBalance})
+details6.append(personBalance)
